5738130a6ab5a6fb1fdbf2fc81b025f81b1d7815
Franck Salzard <franck.salzard@etu.univ-grenoble-alpes.f>
2023-02-02 21:45:52 +0100

    ok

--- lib/src/pages/user.dart
+++ lib/src/pages/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:footer/footer.dart';
 import '../widgets/theappbar.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -17,7 +18,7 @@ class User extends StatelessWidget {
         appBar: CustomAppBar(),
         body: SingleChildScrollView(
           child: Container(
-            height: 1000,
+            //height: 1000,
             constraints: const BoxConstraints(maxWidth: 2000),
             child: Column(
               children: <Widget>[
@@ -49,14 +50,19 @@ class User extends StatelessWidget {
                   ],
                 ),
                 SizedBox(
-                    height: 400,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 500,
+                    //height: 400,
+                    child: Container(
+                  color: Color.fromARGB(255, 194, 204, 204),
+                  child: Row(
+                    children: [
+                      SizedBox(width: width * 0.05),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          width: width * 0.25,
                           padding: const EdgeInsets.all(16.0),
                           decoration: const BoxDecoration(
-                              color: Color.fromARGB(33, 33, 33, 33),
+                              color: Color.fromARGB(255, 217, 239, 240),
                               borderRadius:
                                   BorderRadius.all(Radius.circular(20))),
                           child: Column(
@@ -64,13 +70,14 @@ class User extends StatelessWidget {
                               const Align(
                                 alignment: Alignment.centerLeft,
                                 child: Text(
-                                  "Please, make sure your infomation are up to date",
+                                  "Please, make sure your infomation are up to date :",
                                   style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 24,
-                                  ),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                      color: Color.fromARGB(255, 0, 119, 107)),
                                 ),
                               ),
+                              SizedBox(height: 20),
                               const TextField(
                                 decoration:
                                     InputDecoration(labelText: 'Username'),
@@ -89,15 +96,87 @@ class User extends StatelessWidget {
                               ElevatedButton(
                                 onPressed: () {},
                                 child: Text('Save'),
-                              ),
-                              const SizedBox(
-                                height: 15,
+                                style: ElevatedButton.styleFrom(
+                                    textStyle: const TextStyle(fontSize: 20),
+                                    backgroundColor:
+                                        Color.fromARGB(255, 0, 119, 107)),
                               ),
                             ],
                           ),
                         ),
+                      ),
+                      SizedBox(width: 0.10 * width),
+                      SizedBox(
+                        width: width * 0.5,
+                        child: Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Text(
+                            '"We value your privacy at the highest level, thus you can modify your informations at all time. Please make sure they are correct"',
+                            style: GoogleFonts.varela(
+                                textStyle: const TextStyle(
+                              fontSize: 38,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.normal,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            )),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+                Footer(
+                    backgroundColor: const Color(0xffd9eff0),
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Projet développement d\'applications communicantes',
+                          style: GoogleFonts.varela(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal,
+                            color: Color(0xFF263b5e),
+                          )),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'ENSIMAG 3A IF I2MF',
+                          style: GoogleFonts.varela(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal,
+                            color: Color(0xFF263b5e),
+                          )),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Promotion 2023',
+                          style: GoogleFonts.varela(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal,
+                            color: Color(0xFF263b5e),
+                          )),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Tous droits réservés',
+                          style: GoogleFonts.varela(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal,
+                            color: Color(0xFF263b5e),
+                          )),
+                          textAlign: TextAlign.center,
+                        )
                       ],
-                    ))
+                    )),
               ],
             ),
           ),
