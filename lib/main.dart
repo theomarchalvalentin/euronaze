1b671c38df4a7b1a670cf87c81c4dad8155b3b20
theomarchalvalentin <theo.marchal@etu.emse.fr>
2023-02-07 16:15:45 +0100

    homepage final

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:projet_dac/src/pages/library.dart';
 import 'package:projet_dac/src/pages/user.dart';
 //import 'package:projet_dac/src/widgets/searchbar.dart';
 import 'package:projet_dac/src/pages/home_page.dart';
+import 'src/pages/home_page2.dart';
 import 'src/pages/product_details_screen.dart';
 import 'package:projet_dac/src/pages/login.dart';
 
@@ -47,7 +48,11 @@ final _router = GoRouter(
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => const HomePage(),
+      builder: (context, state) => const HomePage2(),
+    ),
+    GoRoute(
+      path: '/home2',
+      builder: (context, state) => const HomePage2(),
     ),
     GoRoute(
       path: '/user',
