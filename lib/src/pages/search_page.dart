fdce6d729f94e1327ce2e9636811f28f9460a840
theomarchalvalentin <theo.marchal@etu.emse.fr>
2023-02-07 10:09:15 +0100

    search page ui ok

--- lib/src/pages/search_page.dart
+++ lib/src/pages/search_page.dart
@@ -79,114 +79,126 @@ class _SearchPageState extends State<SearchPage> {
           padding: const EdgeInsets.all(40.0),
           child: Column(
             children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Recherche de produits:  ',
-                    style: GoogleFonts.varela(
-                        textStyle: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w600,
-                      fontStyle: FontStyle.normal,
-                      color: Color(0xFF263b5e),
-                    )),
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    style: GoogleFonts.varela(
-                        textStyle: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w200,
-                      fontStyle: FontStyle.normal,
-                      color: Color(0xFF263b5e),
-                    )),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              Divider(
-                color: Colors.black,
-              ),
-              SizedBox(height: 50),
               Container(
-                height: MediaQuery.of(context).size.height * 0.5,
+                height: MediaQuery.of(context).size.height * 0.7,
                 child: Row(
                   children: [
-                    Card(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Search products by name: ',
-                              style: GoogleFonts.varela(
-                                  textStyle: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                fontStyle: FontStyle.normal,
-                                color: Color(0xFF263b5e),
-                              )),
-                            ),
-                            SizedBox(height: 30),
-                            Center(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                      color: Colors.black,
+                    Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              RichText(
+                                textAlign: TextAlign.start,
+                                text: TextSpan(
+                                  text: 'Recherche ',
+                                  style: GoogleFonts.varela(
+                                      textStyle: const TextStyle(
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.w600,
+                                    fontStyle: FontStyle.normal,
+                                    color: Color(0xFF263b5e),
+                                  )),
+                                  children: [
+                                    TextSpan(
+                                      text: 'de produits: ',
+                                      style: GoogleFonts.varela(
+                                          textStyle: const TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w500,
+                                        fontStyle: FontStyle.normal,
+                                        color: Color(0xFF263b5e),
+                                      )),
                                     ),
-                                    borderRadius: BorderRadius.circular(20)),
-                                width: MediaQuery.of(context).size.width * 0.33,
-                                child: TextField(
-                                  controller: _searchController,
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    iconColor: null,
-                                    focusColor: null,
-                                    fillColor: null,
-                                    hoverColor: null,
-                                    prefixIconColor: null,
-                                    hintText: 'Search',
-                                    prefixIcon: Icon(Icons.search),
-                                  ),
-                                  onChanged: _filterProducts,
+                                  ],
                                 ),
                               ),
-                            ),
-                            SizedBox(height: 50),
-                            Text(
-                              'Filter by category: ',
-                              style: GoogleFonts.varela(
-                                  textStyle: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                fontStyle: FontStyle.normal,
-                                color: Color(0xFF263b5e),
-                              )),
-                            ),
-                            SizedBox(height: 20),
-                            Container(
-                              margin: EdgeInsets.only(left: 16),
-                              child: DropdownButton(
-                                value: selectedCategory,
-                                items: _dropdownItems,
-                                onChanged: (value) {
-                                  setState(() {
-                                    selectedCategory = value!;
-                                    _filterProducts(_searchController.text);
-                                  });
-                                },
+                              Divider(
+                                color: Colors.black,
                               ),
-                            ),
-                          ],
+                              SizedBox(height: 30),
+                            ],
+                          ),
                         ),
-                      ),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Rechercher par nom:',
+                                  style: GoogleFonts.varela(
+                                      textStyle: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.normal,
+                                    color: Color(0xFF263b5e),
+                                  )),
+                                ),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.black,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    width: MediaQuery.of(context).size.width *
+                                        0.33,
+                                    child: TextField(
+                                      controller: _searchController,
+                                      decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        iconColor: null,
+                                        focusColor: null,
+                                        fillColor: null,
+                                        hoverColor: null,
+                                        prefixIconColor: null,
+                                        hintText: 'Rechercher',
+                                        prefixIcon: Icon(Icons.search),
+                                      ),
+                                      onChanged: _filterProducts,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 50),
+                                Text(
+                                  'Filtrer par catégorie: ',
+                                  style: GoogleFonts.varela(
+                                      textStyle: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.normal,
+                                    color: Color(0xFF263b5e),
+                                  )),
+                                ),
+                                SizedBox(height: 20),
+                                Container(
+                                  margin: EdgeInsets.only(left: 16),
+                                  child: DropdownButton(
+                                    value: selectedCategory,
+                                    items: _dropdownItems,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        selectedCategory = value!;
+                                        _filterProducts(_searchController.text);
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.04,
