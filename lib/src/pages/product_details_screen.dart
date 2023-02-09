13202733b7adc0046d8d2c4af0f76ab2ff54a71d
theomarchalvalentin <theo.marchal@etu.emse.fr>
2023-02-09 07:40:04 +0100

    laksd

--- lib/src/pages/product_details_screen.dart
+++ lib/src/pages/product_details_screen.dart
@@ -107,6 +107,7 @@ class _ProductScreenDetailsState extends State<ProductScreenDetails> {
                             ),
                           ),
                           Container(
+                            width: 300,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(10),
                                 color: Colors.grey.shade200),
